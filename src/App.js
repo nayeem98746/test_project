fbe6502abe2e6f90ab6dbdba210db5f1bf683f18
nayeem98746 <sabbirahmednayeem941@gmail.com>
2024-11-18 09:09:36 +0600

    changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="test_project_main">
+      <div className="test_project_info">
+        <div className="top_left"></div>
+        <div className="top_right"></div>
+        <div className="bottom_left"></div>
+        <div className="bottom_right"></div>
+        <div className="project_details">
+          <h4>NEW EVENT</h4>
+          <h2>1293,32</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
